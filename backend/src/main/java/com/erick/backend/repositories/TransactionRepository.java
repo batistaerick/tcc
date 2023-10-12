6b45daf61927ae2f66340c671d265e09c71ee008
Erick <batista.erick@outlook.com>
2023-10-12 17:24:26 -0300

    feat: moved the README file

--- backend/src/main/java/com/erick/backend/repositories/TransactionRepository.java
+++ backend/src/main/java/com/erick/backend/repositories/TransactionRepository.java
@@ -2,29 +2,28 @@ package com.erick.backend.repositories;
 
 import com.erick.backend.domains.entities.Transaction;
 import com.erick.backend.enums.TransactionType;
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TransactionRepository
-        extends JpaRepository<Transaction, UUID> {
+    extends JpaRepository<Transaction, UUID> {
     List<Transaction> findByUserEmailAndDateBetween(
-            String userEmail,
-            LocalDate startDate,
-            LocalDate endDate
+        String userEmail,
+        LocalDate startDate,
+        LocalDate endDate
     );
 
     List<Transaction> findByUserEmailAndTransactionTypeAndDateBetween(
-            String userEmail,
-            TransactionType transactionType,
-            LocalDate startDate,
-            LocalDate endDate
+        String userEmail,
+        TransactionType transactionType,
+        LocalDate startDate,
+        LocalDate endDate
     );
 
     List<Transaction> findByUserEmailAndTransactionType(
-            String userEmail,
-            TransactionType transactionType
+        String userEmail,
+        TransactionType transactionType
     );
 }
