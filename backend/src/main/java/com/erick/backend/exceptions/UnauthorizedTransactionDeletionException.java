9bc2729b1f1fbe61566537063a7cd68abb2e4ea4
Erick <batista.erick@outlook.com>
2023-10-26 23:29:05 -0300

    fix: fixed Message formatter

--- backend/src/main/java/com/erick/backend/exceptions/UnauthorizedTransactionDeletionException.java
+++ backend/src/main/java/com/erick/backend/exceptions/UnauthorizedTransactionDeletionException.java
@@ -1,11 +1,13 @@
 package com.erick.backend.exceptions;
 
+import org.slf4j.helpers.MessageFormatter;
+
 public class UnauthorizedTransactionDeletionException extends RuntimeException {
 
     public UnauthorizedTransactionDeletionException(
         String message,
         Object... objects
     ) {
-        super(String.format(message, objects));
+        super(MessageFormatter.arrayFormat(message, objects).getMessage());
     }
 }
