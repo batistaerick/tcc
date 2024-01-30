bd626a8267648124dd57155a780eb76a84f6118a
Erick <batista.erick@outlook.com>
2024-01-30 00:45:09 -0300

    test: fixed the test

--- frontend/__test__/components/DatePickerDialog.spec.tsx
+++ frontend/__test__/components/DatePickerDialog.spec.tsx
@@ -4,12 +4,11 @@ import { render } from '@testing-library/react';
 describe('DatePickerDialog component', () => {
   it('should renders the date picker with the initial date', () => {
     const setDate = jest.fn();
-    const { getByDisplayValue } = render(
-      <DatePickerDialog date={new Date('2022-01-01')} setDate={setDate} />
+    const { getByRole } = render(
+      <DatePickerDialog date={new Date('2024-05-05')} setDate={setDate} />
     );
 
-    const inputElement = getByDisplayValue('31/December/2021'); // Update the expected date format
-
+    const inputElement = getByRole('textbox');
     expect(inputElement).toBeInTheDocument();
   });
 });
