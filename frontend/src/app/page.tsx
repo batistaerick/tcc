1dc8511d7ab17c877dae0ff8437358ead59c2d06
Erick Batista <77533954+batistaerick@users.noreply.github.com>
2024-02-28 23:13:43 -0300

    Dev (#29)
    * feat: add better transactions request
    
    * feat: add tremor

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -9,21 +9,32 @@ export default function Home() {
   const { push } = useRouter();
   const { t } = useTranslation();
 
-  const HeaderComponent = dynamic(() => import('@/components/Header'));
-  const BalanceComponent = dynamic(() => import('@/components/Balance'));
-  const TransactionsComponent = dynamic(
-    () => import('@/components/Transactions')
+  const DynamicHeader = dynamic(() => import('@/components/Header'), {
+    loading: () => <header className="h-12 w-10/12 rounded-xl" />,
+  });
+  const DynamicBalance = dynamic(() => import('@/components/Balance'), {
+    loading: () => (
+      <div className="h-52 w-10/12 cursor-default rounded-xl bg-slate-700 bg-opacity-60" />
+    ),
+  });
+  const DynamicTransactions = dynamic(
+    () => import('@/components/Transactions'),
+    {
+      loading: () => (
+        <div className="h-[252px] w-10/12 rounded-xl bg-blue-950 bg-opacity-65" />
+      ),
+    }
   );
-  const ButtonComponent = dynamic(() => import('@/components/Button'));
+  const DynamicButton = dynamic(() => import('@/components/Button'));
 
   return (
     <DefaultBackground>
       <div className="w-10/12">
-        <HeaderComponent dateFormat="MMM/yyyy" />
+        <DynamicHeader dateFormat="MMM/yyyy" />
       </div>
-      <BalanceComponent />
-      <TransactionsComponent />
-      <ButtonComponent
+      <DynamicBalance />
+      <DynamicTransactions />
+      <DynamicButton
         height="h-12"
         width="w-10/12"
         translation={t('button:newTransaction')}
