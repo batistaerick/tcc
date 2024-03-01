6857f1771dafad95813db9fcd077acd0fcf6a5e7
Erick <batista.erick@outlook.com>
2024-03-01 14:53:48 -0300

    fix: removes the optional from the fetch body

--- frontend/src/libs/fetchers.ts
+++ frontend/src/libs/fetchers.ts
@@ -17,7 +17,7 @@ export async function getFetcher<T>(
 
 export async function postFetcher<T>(
   url: string,
-  body?: T,
+  body: T,
   config?: AxiosRequestConfig
 ): Promise<T> {
   return await defaultAxios
@@ -30,7 +30,7 @@ export async function postFetcher<T>(
 
 export async function putFetcher<T>(
   url: string,
-  body?: T,
+  body: T,
   config?: AxiosRequestConfig
 ): Promise<T> {
   return await defaultAxios
